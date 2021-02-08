9a5d14fabb42794c84169425846e2b9e1e4c54c1
Nina Jade <jaderiver64@gmail.com>
2021-02-07 23:29:05 -0600

    inquirer-loop added and better questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,108 +1,113 @@
 const inquirer = require("inquirer");
+inquirer.registerPrompt("loop", require("inquirer-loop")(inquirer));
 
-// TODO: Include packages needed for this application
 
-var output = [];
-// const fs = require('fs');
-// const { writeFile, copyFile } = require('./src/generateMarkdown');
-// const generateREADME = require('./src/markdown-template');
-// Develop/index.js
-// TODO: Create an array of questions for user input
-const  questions = [
-    {
+//TODO: Include packages needed
+//for this application
+
+
+//const fs = require('fs');
+//const { writeFile, copyFile } = require('./src/generateMarkdown');
+//const generateREADME = require('./src/markdown-template');
+//Develop / index.js
+//TODO: Create an array of questions
+//for user input
+const questions = [{
         type: 'input',
-        name:'name',
+        name: 'name',
         message: 'Who is the creator of this project? (Required)',
         validate: nameInput => {
             if (nameInput) {
-              return true;
+                return true;
             }
-              console.log('Please enter a name!');
-              return false;
-          }
+            console.log('Please enter a name!');
+            return false;
+        }
     },
     {
         type: 'input',
-        name:'title',
+        name: 'title',
         message: 'What is the project\'s title? (Required)',
         validate: nameInput => {
             if (nameInput) {
-              return true;
+                return true;
             }
-              console.log('Please enter a title!');
-              return false;
-          }
+            console.log('Please enter a title!');
+            return false;
+        }
     },
     {
         type: 'input',
         name: 'github',
         message: 'Enter this project\'s GitHub Username (Required)',
         validate: githubInput => {
-          if (githubInput) {
-            return true;
-          }
+            if (githubInput) {
+                return true;
+            }
             console.log('Please enter a GitHub username!');
             return false;
         }
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'link',
         message: 'Enter the GitHub link to the project: (Required)',
         validate: linkInput => {
-          if (linkInput) {
-            return true;
-          }
-        console.log('You need to enter a project GitHub link!');
-        return false;
+            if (linkInput) {
+                return true;
+            }
+            console.log('You need to enter a project GitHub link!');
+            return false;
         }
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'description',
         message: 'Provide some information about this project for the README:',
         validate: descriptionInput => {
             if (descriptionInput) {
-              return true;
+                return true;
             }
             console.log('Please enter some description of the project!');
             return false;
         }
-      },
-      {
-          type: 'confirm',
-          name: 'table-of-contents',
-          message: 'Would you like to include a Table of Contents?',
-          validate: (tableOption)=>{
-              if(tableOption){
-                  return true;
-              }
-              return false;
-          }
-      },
-      {
-        type: 'confirm',
-        name: 'install-query',
-        message: 'Would you like to include installation information?',
-        validate: (tableOption)=>{
-            if(tableOption){
-                return true;
-            }
-            return false;
-        }
     },
-      {
-        type: 'editor',
-        name: 'installation',
-        message: 'Please enter installation steps in the editor:'
-      },
+    {
+        type: 'confirm',
+        name: 'table-of-contents',
+        message: 'Would you like to include a Table of Contents?',
+    },
+    {
+        type: 'confirm',
+        name: 'installQuery',
+        message: 'Would you like to include installation information?',
+        default: true
+
+    },
+    {
+        type: 'loop',
+        name: 'installItems',
+        message: 'Add an installation step?',
+        when: ({ installQuery }) => installQuery,
+        questions: [{
+                type: "input",
+                name: "step",
+                message: "Enter an installation step title:",
+            },
+            {
+                type: "input",
+                name: "stepDescription",
+                message: "Enter the description of this installation step:",
+            },
+        ],
+    }
 
 ];
 
 
-  inquirer.prompt(questions).then((answers) => {
+inquirer.prompt(questions).then((answers) => {
     console.log(JSON.stringify(answers, null, '  '));
-  });
+});
 
 // // THEN a high-quality, professional README.md is generated with the title of my project and sections
 // // entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
@@ -115,3 +120,20 @@ const  questions = [
 
 // // // Function call to initialize app
 // // init();
+
+//inquirer.prompt({
+//    type: "loop",
+//    name: "items",
+//    message: "Add another item?",
+//    questions: [{
+//            type: "input",
+//            name: "key",
+//            message: "Enter item key",
+//        },
+//        {
+//            type: "input",
+//            name: "value",
+//            message: "Enter item value",
+//        },
+//    ],
+//});
