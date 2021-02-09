30ad01230db736518413bdd89431b721b91506fd
Nina Jade <jaderiver64@gmail.com>
2021-02-09 09:39:49 -0600

    more testing!

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -70,23 +70,29 @@ module.exports = generateMarkdown;
 
 
 //{
-//    "name": "nina",
-//    "title": "meow",
-//    "github": "meow",
-//    "link": "meow",
-//    "description": "dope",
-//    "tableOfContents": true,
-//    "installation": "meows",
-//    "license": [
-//        "GNU AGPLv3"
-//    ],
-//    "usage": "meow",
-//    "imageUrl": "meow",
-//    "contactName": "meow",
-//    "email": "jaderiver64@gmail.com",
-//    "test": "meow",
-//    "credits": [{
-//        "contributor": "nuggles",
-//        "contributorLink": "https://nugglesworld.com"
-//    }]
+//    name: 'Nina Cummings',
+//    title: 'aide-de-camp',
+//    github: 'jaderiver64',
+//    link: 'https://github.com/jaderiver62/aide-de-camp',
+//    description: 'This is an app in Node.js that creates a quality README.md file
+//    for projects!This is still a work in progess so please forgive my tests.Watc
+//    h this space!',
+//    tableOfContents: true,
+//    installation: 'installationsteps to be provided soon!',
+//    license: 'MIT License',
+//    languages: ['JavaScript', 'ES6', 'Node'],
+//    usage: 'Examples and Intructions coming soon!',
+//    imageUrl: 'Develop/utils/tests/images/nuggles.jpg',
+//    contactName: 'jaderiver62',
+//    email: 'jaderiver64@gmail.com',
+//    test: 'Testing still in progress!  Tune in soon!',
+//    credits: [{
+//            contributor: 'Nuggles the Cat',
+//            contributorLink: 'https://github.com/jaderiver62/'
+//        },
+//        {
+//            contributor: 'Jadoo the Cat',
+//            contributorLink: 'https://github.com/jaderiver62/'
+//        }
+//    ]
 //}
--- README.md
+++ README.md
@@ -1,25 +1,26 @@
 
-# my baby
+# aide-de-camp
 ## Description
-meows
+This is an app that helps a user create a quality README.md file for thei project.  This is still in testing mode so please come back soon!
 
 
 
-** [baby butt](https://github.com/jaderiver62/aide-de-camp/)
+** [jaderiver62](https://github.com/jaderiver62/aide-de-camp)
 ## Table of Contents
 * [Installation](#installation)
 * [Usage](#usage)
 * [License](#license)
 * [Credits](#credits)
-
 ## Installation
-meow
+    Coming soon!
 
-## Usage 
-meowssss
-![Project Usage Image](Develop/utils/tests/images/nuggles.jpg)
-
-
-
+    ## Usage 
+     Coming soon!
+    ![Project Usage Image](Develop/utils/tests/images/nuggles.jpg)
+    
+    
+        ![Nuggles the Cat](https://github.com/jaderiver62/)
         
- 
+        ![Jadoo the Cat](https://github.com/jaderiver62/)
+        
+    
